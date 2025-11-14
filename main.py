1e7e09317b7a96e5044e0e6201ed1b33b3d89a3c
RomanchukRoman <r.romanchuk@ya.ru>
2025-11-14 23:10:58 +0300

    fix main functions

--- main.py
+++ main.py
@@ -11,3 +11,8 @@ if __name__ == '__main__':
 # register --username alice --password 1234
 # login --username alice --password 1234
 # show-portfolio
+# buy --currency BTC --amount 0.05
+# buy --currency USD --amount 1000
+# sell --currency BTC --amount 0.0001
+# sell --currency USD --amount 100
+# get-rate --from USD --to BTC
